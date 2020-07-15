ffd0f7e1092722bf9ebd3d9dd2f97cfb85e22630
Reto Ischi <reto.ischi@ergon.ch>
2020-07-15 16:48:33 +0200

    show option added

--- gateway_rest_api/ip_list.py
+++ gateway_rest_api/ip_list.py
@@ -3,6 +3,7 @@
 """
 Version 1.1
 Script to manage IP list usages
+All actions operate on the newest saved or active configuration.
 """
 
 from urllib import request
@@ -59,7 +60,8 @@ if args.log_only and args.iplist is None:
 TARGET_GATEWAY = "https://{}".format(args.host)
 
 try:
-    api_key = args.api_key if args.api_key else open(API_KEY_FILE, 'r').read().strip()
+    api_key = (args.api_key if args.api_key
+               else open(API_KEY_FILE, 'r').read().strip())
 except IOError:
     print("Please write the Airlock Gateway API key into file {}"
           .format(API_KEY_FILE))
@@ -92,87 +94,94 @@ def terminate_and_exit(text):
     sys.exit(text)
 
 
-# create session
-send_request("POST", "session/create")
-
-
 # signal handler
-def cleanup(signum, frame):
-    terminate_and_exit("Terminate session")
+def register_cleanup_handler():
+    def cleanup(signum, frame):
+        terminate_and_exit("Terminate session")
+
+    for sig in (signal.SIGABRT, signal.SIGILL, signal.SIGINT, signal.SIGSEGV,
+                signal.SIGTERM):
+        signal.signal(sig, cleanup)
 
 
-for sig in (signal.SIGABRT, signal.SIGILL, signal.SIGINT, signal.SIGSEGV,
-            signal.SIGTERM):
-    signal.signal(sig, cleanup)
+def load_last_config():
+    resp = json.loads(send_request("GET", "configuration/configurations"))
+    send_request("POST", "configuration/configurations/{}/load"
+                         .format(resp['data'][0]['id']))
 
-# get last config (active or saved)
-resp = json.loads(send_request("GET", "configuration/configurations"))
-send_request("POST", "configuration/configurations/{}/load"
-                     .format(resp['data'][0]['id']))
 
-# get all mappings
-resp_all_mappings = json.loads(send_request("GET", "configuration/mappings"))
+def get_all_mappings():
+    return json.loads(send_request("GET", "configuration/mappings"))
 
-# filter mappings
-mapping_ids = (
-    [x['id'] for x in resp_all_mappings['data']
-        if(re.search(args.mapping_selector_pattern, x['attributes']['name']))]
-    if args.mapping_selector_pattern
-    else [x['id'] for x in resp_all_mappings['data']
-          if(args.mapping_selector_label in x['attributes']['labels'])])
-mapping_names = [x['attributes']['name'] for x in resp_all_mappings['data']
-                 if(x['id'] in mapping_ids)]
 
-if not mapping_ids:
-    terminate_and_exit("No mapping found - exit")
+def filter_mappings(all_mappings):
+    # filter mappings
+    return ([
+        {'id': x['id'], 'name': x['attributes']['name']}
+        for x in all_mappings['data'] if (
+            args.mapping_selector_pattern and
+            re.search(args.mapping_selector_pattern, x['attributes']['name']) or
+            args.mapping_selector_label in x['attributes']['labels']
+            )
+        ])
 
-# get all ip lists
-resp = json.loads(send_request("GET", "configuration/ip-address-lists"))
 
-ip_list_ids = []
-# filter ip lists
-if args.iplist:
-    ip_list_ids = [x['id'] for x in resp['data']
-                if(re.search(args.iplist, x['attributes']['name']))]
-    ip_list_names = [x['attributes']['name'] for x in resp['data']
-                    if(x['id'] in ip_list_ids)]
-    if not ip_list_ids:
-        terminate_and_exit("IP list matching '{}' not found".format(args.iplist))
+def get_ip_lists():
+    # get all ip lists
+    resp = json.loads(send_request("GET", "configuration/ip-address-lists"))
 
-list_type = "blacklists" if args.blacklist else "whitelists"
+    ip_lists = []
+    if args.iplist:
+        ip_lists = ([
+            {'id': x['id'], 'name': x['attributes']['name']}
+            for x in resp['data'] if (
+                re.search(args.iplist, x['attributes']['name'])
+            )
+        ])
+    return ip_lists
 
-# show ip list usage and terminate
-if args.action == "show":
-    for mapping in resp_all_mappings['data']:
-        print(mapping['attributes']['ipRules']['ipAddressWhitelists']['logOnly'])
+
+def show_usages(all_mappings, mappings, list_type, ip_lists):
+    dump = {}
+    for mapping in all_mappings['data']:
+        ipr = mapping['attributes']['ipRules']
+        dump[mapping['id']] = {
+            'ip_lists': [],
+            'log_only': ipr['ipAddress{}'.format(list_type.title())]['logOnly']
+            }
+
     resp = json.loads(send_request("GET", "/configuration/ip-address-lists"))
     for r in resp['data']:
-        print(r['relationships']['ip-address-whitelists'])
-    #for mapping in list(itertools.product(mapping_ids, mapping_names)):
-    #    print(mapping[1])
-    #    resp = send_request("GET",
-    #            "configuration/mappings/{}/relationships/ip-address-{}"
-    #            .format(mapping[1], list_type))
-    #    print(resp)
-    #for ip_list in list(itertools.product(ip_list_ids, ip_list_names)):
-    #    print(ip_list[1] + "\t")
+        for m in r['relationships'][f'ip-address-{list_type}']['data']:
+            if m['id'] in dump:
+                dump[m['id']]['ip_lists'].append(r['id'])
+
+    for mapping_id, mapping_ip_list in list(dump.items()):
+        print('{}'.format(list(m['name'] for m in mappings
+                        if m['id'] == mapping_id)[0]))
+        print('\tlog-only: {}'.format(mapping_ip_list['log_only']))
+        print('\tIP {}: {}'.format(list_type, ', '.join(
+            list(ipl['name'] for ipl in ip_lists if ipl['id'] == mipl)[0]
+            for mipl in mapping_ip_list['ip_lists'])))
 
     terminate_and_exit(0)
 
 
-for mapping_id in mapping_ids:
-    resp = json.loads(send_request("GET", "/configuration/mappings/{}"
-                                          .format(mapping_id)))
+def patch_config(mappings, list_type, ip_lists):
+    for mapping in mappings:
+        resp = json.loads(send_request("GET", "/configuration/mappings/{}"
+                                              .format(mapping['id'])))
 
     selected_ip_list = []
-    for ip_list_id in ip_list_ids:
-        selected_ip_list.append({"id": ip_list_id, "type": "ip-address-list"})
+    for ip_list in ip_lists:
+        selected_ip_list.append({"id": ip_list['id'],
+                                "type": "ip-address-list"})
 
     data = {"data": selected_ip_list}
     method = "PATCH" if args.action == "add" else "DELETE"
     send_request(method,
-                 "configuration/mappings/{}/relationships/ip-address-{}"
-                 .format(mapping_id, list_type), json.dumps(data))
+                "configuration/mappings/{}/relationships/ip-address-{}"
+                .format(mapping['id'], list_type), json.dumps(data))
     if args.log_only:
         data = {
             "data": {
@@ -191,29 +200,79 @@ for mapping_id in mapping_ids:
         send_request("PATCH", "configuration/mappings/{}"
                      .format(mapping_id), json.dumps(data))
 
-change_info = ''
-if args.iplist:
-    change_info += '{} {} group(s) "{}"'.format(args.action, list_type[:-1],
-                  ', '.join(ip_list_names))
+
+def create_change_info(mappings, list_type, ip_lists):
+    change_info = ''
+    if args.iplist:
+        change_info += '{} {} group(s) "{}"'\
+                       .format(args.action, list_type[:-1],
+                               ', '.join(x['name'] for x in ip_lists))
+        if args.log_only:
+            change_info += ' and '
     if args.log_only:
-        change_info += ' and '
-if args.log_only:
-    change_info += 'set log_only to "{}"'.format(args.log_only)
-change_info += ' for the following mapping(s): \n{}'.format('\n\t'.join(sorted(mapping_names)))
-if args.confirm:
-    print(change_info)
-    if input('\nContinue to save the config? [y/n] ') != 'y':
-        print("Nothing changed")
-        terminate_and_exit(0)
+        change_info += 'set log_only to "{}"'.format(args.log_only)
+    change_info += ' for the following mapping(s): \n\t{}'\
+                   .format('\n\t'.join(sorted(x['name'] for x in mappings)))
+    return change_info
 
-data = {"comment": "REST: " + change_info.replace('\n','').replace('\t',',')}
 
-# save config
-send_request("POST", "configuration/configurations/save", json.dumps(data))
-print('Config saved with comment: {}'.format(data['comment']))
+def confirm(change_info):
+    if args.confirm:
+        print(change_info)
+        if input('\nContinue to save the config? [y/n] ') == 'y':
+            return True
+        else:
+            print("Nothing changed")
+            return False
 
-# activate config without failover activation!
-# send_request("POST", "configuration/configurations/activate",
-#              json.dumps(data))
 
-terminate_and_exit(0)
+def save_config(change_info):
+    data = {"comment": "REST: " + change_info.replace('\n', '').replace('\t', ',')}
+
+    # save config
+    send_request("POST", "configuration/configurations/save", json.dumps(data))
+    print('Config saved with comment: {}'.format(data['comment']))
+
+
+def activate_config():
+    nop
+    # activate config without failover activation!
+    # send_request("POST", "configuration/configurations/activate",
+    #              json.dumps(data))
+
+
+def main():
+    # create session
+    send_request("POST", "session/create")
+    register_cleanup_handler()
+
+    # load last config (active or saved)
+    load_last_config()
+
+    all_mappings = get_all_mappings()
+    # filter mappings
+    mappings = filter_mappings(all_mappings)
+    if not mappings:
+        terminate_and_exit("No mapping found - exit")
+
+    # filter ip lists
+    ip_lists = get_ip_lists()
+    if not ip_lists:
+        terminate_and_exit("IP list matching '{}' not found"
+                           .format(args.iplist))
+
+    list_type = "blacklists" if args.blacklist else "whitelists"
+
+    # show ip list usage and terminate
+    if args.action == "show":
+        show_usages(all_mappings, mappings, list_type, ip_lists)
+
+    patch_config(mappings, list_type, ip_lists)
+    change_info = create_change_info(mappings, list_type, ip_lists)
+    confirm(change_info) or terminate_and_exit(0)
+    save_config(change_info)
+    terminate_and_exit(0)
+
+
+if __name__ == '__main__':
+    main()
